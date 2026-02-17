db5d018cd2c013f04717f8a84b55f9e59a66e8b2
Tasneem424 <neemo56785678@gmail.com>
2026-02-17 02:25:51 +0200

    final_pro

---
+++ ML/guitk.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import numpy as np
+
+class salary_pred():
+     def __init__(self,root):
+          self.root=root
+          self.root.title("Depi")
+          self.root.
+
+if __name__ == "__main__":
+     root= tk.Tk()
+     app= salary_pred(root)
+     root.mainloop()
